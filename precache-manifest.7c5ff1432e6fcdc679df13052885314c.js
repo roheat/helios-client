996c45e9a25d71e0e1fa8af79a245e09dac512bd
roheat <rohithsrivathsav@gmail.com>
2019-12-11 17:58:32 +0530

    Updates

---
+++ precache-manifest.7c5ff1432e6fcdc679df13052885314c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2f5604f9b5bcb585fd1e6e33ead309a",
+    "url": "/helios-client/index.html"
+  },
+  {
+    "revision": "2077e1632d3eab8c10b7",
+    "url": "/helios-client/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "80d09b40eb32a87816c2",
+    "url": "/helios-client/static/css/main.3f999912.chunk.css"
+  },
+  {
+    "revision": "2077e1632d3eab8c10b7",
+    "url": "/helios-client/static/js/2.44e06618.chunk.js"
+  },
+  {
+    "revision": "0f0dcfea74811c7cc4b97a923d4765a7",
+    "url": "/helios-client/static/js/2.44e06618.chunk.js.LICENSE"
+  },
+  {
+    "revision": "80d09b40eb32a87816c2",
+    "url": "/helios-client/static/js/main.4deca51f.chunk.js"
+  },
+  {
+    "revision": "8140b7502bf1843d484b",
+    "url": "/helios-client/static/js/runtime-main.f71c474a.js"
+  }
+]);
