1d95f7d179fb6b7523da5763f67141c0f867aab6
roheat <rohithsrivathsav@gmail.com>
2019-12-11 18:55:15 +0530

    changes new

--- src/App.js
+++ src/App.js
@@ -10,16 +10,8 @@ function App() {
   return (
     <div className="App">
       <HashRouter>
-        <Route
-          path={process.env.PUBLIC_URL + "/"}
-          exact
-          component={LandingPage}
-        />
-        <Route
-          path={process.env.PUBLIC_URL + "/platform"}
-          exact
-          component={TradingPlatform}
-        />
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/platform" exact component={TradingPlatform} />
       </HashRouter>
     </div>
   );
