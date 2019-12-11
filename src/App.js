0a3f178c00a0db8e21bc4b78445c3f170e07a35d
roheat <rohithsrivathsav@gmail.com>
2019-12-11 18:58:21 +0530

    changes neww

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
-      <HashRouter>
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <Route path="/" exact component={LandingPage} />
         <Route path="/platform" exact component={TradingPlatform} />
       </HashRouter>
